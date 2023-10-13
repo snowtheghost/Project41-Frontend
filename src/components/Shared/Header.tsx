2a0b9f5544e4e7c3c8a340d30fdc21d35fef8e8f
Cecil Cao <pokemonball34@gmail.com>
2023-10-13 00:57:54 -0400

    Clean up Login Screen

--- src/components/Shared/Header.tsx
+++ src/components/Shared/Header.tsx
@@ -1,5 +1,7 @@
+import { Link as RouterLink } from 'react-router-dom';
+
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
 
 type Props = {
   title: String;
@@ -13,19 +15,22 @@ const Header = (props: Props) => {
       sx={{
         backgroundColor: '#05004E',
         width: '100%',
-        minHeight: '10%',
+        maxHeight: '10%',
+        padding: '1.5rem',
       }}
     >
-      <Typography
+      <Link
+        component={RouterLink}
+        to='/'
         sx={{
           color: '#EBA6A6',
-          padding: '16px',
           fontSize: '1.1rem',
           fontWeight: 800,
+          textDecoration: 'none',
         }}
       >
         {title}
-      </Typography>
+      </Link>
     </Box>
   );
 };
