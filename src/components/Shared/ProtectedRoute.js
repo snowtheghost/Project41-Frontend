3f62441219aeb461883aca6f18fe6444990bb5b8
JoAndNight <934569760@qq.com>
2023-10-13 02:05:10 -0400

    Linters cleaned up

--- src/components/Shared/ProtectedRoute.js
+++ src/components/Shared/ProtectedRoute.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { Route, Navigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { isLoggedIn } from '../../utils/auth';
 
 const ProtectedRoute = ({ children }) => {
