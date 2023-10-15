baafbfd667e2afa119ad06fb1089695aae9a91ed
JoAndNight <934569760@qq.com>
2023-10-14 23:35:05 -0400

    Set up Participant pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,9 @@ import HomePage from './pages/Home/HomePage';
 import RegisterPage from './pages/UserLogin/RegisterPage';
 import LoginPage from './pages/UserLogin/LoginPage';
 import GameLibraryIndex from './pages/ParticipantView/GameLibrary/GameLibraryIndex';
+import PlayHistory from './pages/ParticipantView/PlayHistory/PlayHistory';
+import Rewards from './pages/ParticipantView/Rewards/Rewards';
+import Profile from './pages/ParticipantView/Profile/Profile';
 
 const App = () => {
   return (
@@ -23,6 +26,9 @@ const App = () => {
           <Route path='/login' element={<LoginPage />} />
           <Route path='/register' element={<RegisterPage />} />
           <Route path='/gamelibrary' element={<GameLibraryIndex/>} />
+          <Route path='/playhistory' element={<PlayHistory/>} />
+          <Route path='/myrewards' element={<Rewards/>} />
+          <Route path='/myprofile' element={<Profile/>} />
           <Route
             path='/funds'
             element={
