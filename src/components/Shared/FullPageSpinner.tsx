0e71e7683f18de44868050c1f917ddadf0016c9f
Cecil Cao <pokemonball34@gmail.com>
2023-10-15 22:16:50 -0400

    FE-0111: Add Prisoner's Dilemma

---
+++ src/components/Shared/FullPageSpinner.tsx
@@ -0,0 +1,7 @@
+import CircularProgress from '@mui/material/CircularProgress';
+
+const FullPageSpinner = () => {
+  return <CircularProgress sx={{ height: '100%', width: '100%' }} />;
+};
+
+export default FullPageSpinner;
---
+++ src/pages/[gameId]/GameClient/GameClient.tsx
@@ -0,0 +1,9 @@
+import Box from '@mui/material/Box';
+
+type Props = {};
+
+const GameClient = (props: Props) => {
+  return <Box></Box>;
+};
+
+export default GameClient;
