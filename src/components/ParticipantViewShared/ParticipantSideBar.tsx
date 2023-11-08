e31878ca77f435629634e219359a9812ddb3e08b
JoAndNight <934569760@qq.com>
2023-11-08 00:31:07 -0500

    Update Sidebr and game library

--- src/components/ParticipantViewShared/ParticipantSideBar.tsx
+++ src/components/ParticipantViewShared/ParticipantSideBar.tsx
@@ -1,44 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
-import * as FaIcons from 'react-icons/fa' 
-
 import { ParticipantSideBarData } from './ParticipantSideBarData';
 
-const Navbar = styled.div`
-    display: flex;
-    justify-content: start;
-    align-items: center;
-    width: 250px;
-    height: 3.5rem;
-    background-color: #05004e;
-`
-
-const MenuIconOpen = styled(Link)`
-    display: flex;
-    justify-content: start;
-    font-size: 1.5rem;
-    margin-left: 0.75rem;
-    color: #f9f8eb;
-`
-
-const MenuIconClose = styled(Link)`
-    display: flex;
-    justify-content: end;
-    font-size: 1.5rem;
-    margin-top: 0.75rem;
-    margin-right: 1rem;
-    color: #f9f8eb;
-`
-
-const SidebarMenu = styled.div<{close: boolean}>`
+const SidebarMenu = styled.div`
+    min-width: 250px;
     width: 250px;
     height:100vh;
     background-color: #eaa6a6;
-    position: fixed;
-    top: calc(10% - 1.5rem);
-    left: ${({ close}) => close ? '0' : '-100%'};
     transition: .6s;
     z-index: 10;
 `
@@ -73,20 +43,9 @@ const MenuItemLinks = styled(Link)`
 `
 
 const ParticipantSideBar: React.FunctionComponent = () => {
-    const [close, setClose] = useState(false)
-    const showSidebar = () => setClose(!close)
     return (
         <>
-            <Navbar>
-                <MenuIconOpen to="#" onClick={showSidebar}>
-                    <FaIcons.FaBars />
-                </MenuIconOpen>
-            </Navbar>
-
-            <SidebarMenu close={close}>
-                <MenuIconClose to="#" onClick={showSidebar}>
-                    <FaIcons.FaTimes />
-                </MenuIconClose>
+            <SidebarMenu>
 
                 {ParticipantSideBarData.map((item, index) => {
                     return (
