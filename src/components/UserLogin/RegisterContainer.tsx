411349d33e336a3ab61c93d72c6a32148abb88de
Cecil Cao <pokemonball34@gmail.com>
2023-11-08 19:58:02 -0500

    FE-0108: Register Page Backbone

--- src/components/UserLogin/RegisterContainer.tsx
+++ src/components/UserLogin/RegisterContainer.tsx
@@ -12,17 +12,19 @@ import Tooltip from '@mui/material/Tooltip';
 export type userType = 'Participant' | 'Researcher';
 
 type Props = {
-  handleLogin: (
+  handleRegister: (
     event: { preventDefault: () => void },
+    username: string,
     email: string,
     password: string,
     userType: userType
   ) => void;
 };
 
-const LoginContainer = (props: Props) => {
-  const { handleLogin } = props;
+const RegisterContainer = (props: Props) => {
+  const { handleRegister } = props;
   const [userType, setUserType] = useState<userType>('Participant');
+  const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isLoggingIn, setIsLoggingIn] = useState(false);
@@ -30,7 +32,7 @@ const LoginContainer = (props: Props) => {
   return (
     <Grid container sx={{ padding: '1rem' }}>
       <Typography variant='h4' sx={{ fontWeight: 800, margin: '1rem' }}>
-        Login As...
+        Register As...
       </Typography>
       <Grid container sx={{ justifyContent: 'center' }}>
         <Grid className='Participant-Button' item xs={6}>
@@ -94,11 +96,30 @@ const LoginContainer = (props: Props) => {
         <Grid container sx={{ justifyContent: 'center', width: '100%' }}>
           <form
             onSubmit={(e) => {
-              handleLogin(e, email, password, userType);
+              handleRegister(e, username, email, password, userType);
               setIsLoggingIn(true);
             }}
             style={{ width: '100%', padding: '1rem' }}
           >
+            <Grid
+              container
+              sx={{ justifyContent: 'space-between', padding: '0.5rem 0' }}
+            >
+              <Typography
+                sx={{ color: '#EBA6A6', fontWeight: 800, margin: '0.25rem' }}
+              >
+                Username:
+              </Typography>
+              <input
+                type='username'
+                name='username'
+                placeholder='Username'
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+                style={{ width: '70%' }}
+              />
+            </Grid>
             <Grid
               container
               sx={{ justifyContent: 'space-between', padding: '0.5rem 0' }}
@@ -146,7 +167,7 @@ const LoginContainer = (props: Props) => {
                 fontWeight: 800,
               }}
             >
-              {isLoggingIn ? <CircularProgress /> : 'Sign In'}
+              {isLoggingIn ? <CircularProgress /> : 'Register'}
             </Button>
           </form>
         </Grid>
@@ -156,11 +177,11 @@ const LoginContainer = (props: Props) => {
           sx={{ color: '#EBA6A6', margin: '1rem', padding: '0.5rem' }}
           to='/register'
         >
-          Don't have an account?
+          Already have an account?
         </Link>
       </Paper>
     </Grid>
   );
 };
 
-export default LoginContainer;
+export default RegisterContainer;
--- src/pages/UserLogin/RegisterPage.tsx
+++ src/pages/UserLogin/RegisterPage.tsx
@@ -1,16 +1,26 @@
 import { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
-import axios from '../../utils/axiosInstance';
+import { useNavigate } from 'react-router-dom';
+
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+
+import axios from 'src/utils/axiosInstance';
+import RegisterContainer, {
+  userType,
+} from 'src/components/UserLogin/RegisterContainer';
 
 const RegisterPage = () => {
   const navigate = useNavigate();
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
   // THIS TYPING IS BAD. FIX THIS.
-  const handleRegister = async (event: { preventDefault: () => void }) => {
+  const handleRegister = async (
+    event: { preventDefault: () => void },
+    username: string,
+    email: string,
+    password: string,
+    userType: userType
+  ) => {
     event.preventDefault();
 
     try {
@@ -33,40 +43,37 @@ const RegisterPage = () => {
   };
 
   return (
-    <div>
-      <h1>Register</h1>
-      <form onSubmit={handleRegister}>
-        <input
-          type='username'
-          name='username'
-          placeholder='Username'
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-        <input
-          type='email'
-          name='email'
-          placeholder='Email'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <input
-          type='password'
-          name='password'
-          placeholder='Password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <button type='submit'>Register</button>
-      </form>
-      {errorMessage && <p>{errorMessage}</p>}
-      <p>
-        Already have an account? <Link to='/login'>Login</Link>
-      </p>
-    </div>
+    <Grid
+      container
+      sx={{
+        display: 'inline-flex',
+        backgroundColor: '#F9F8EB',
+        width: '100%',
+        height: '100%',
+      }}
+    >
+      <Grid
+        item
+        className='body'
+        xs={6}
+        sx={{
+          display: 'inline-grid',
+          justifyContent: 'center',
+          padding: '96px 0 0 0',
+        }}
+      >
+        Main body information
+      </Grid>
+      <Grid
+        item
+        className='sidebar'
+        xs={6}
+        sx={{ justifyContent: 'center', padding: '1rem 4rem 4rem 4rem' }}
+      >
+        <RegisterContainer handleRegister={handleRegister} />
+        {errorMessage && <Typography>{errorMessage}</Typography>}
+      </Grid>
+    </Grid>
   );
 };
 
