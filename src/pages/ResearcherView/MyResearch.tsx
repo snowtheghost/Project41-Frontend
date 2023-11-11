03c2c85585c8e1e97382432894fc8499300ec522
Cecil Cao <pokemonball34@gmail.com>
2023-11-11 00:27:16 -0500

    Implement MyResearch skeleton

--- src/pages/ResearcherView/MyResearch.tsx
+++ src/pages/ResearcherView/MyResearch.tsx
@@ -1,12 +1,24 @@
-import ResearcherSideBar from "src/components/ResearcherViewShared/ResearcherSideBar";
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import ResearcherProfileInfo from 'src/components/ResearcherViewShared/ResearcherProfileInfo';
+
+import ResearcherSideBar from 'src/components/ResearcherViewShared/ResearcherSideBar';
 
 const MyResearch = () => {
-    return (
-      <div style={{ backgroundColor: '#F9F8EB', minHeight: '100vh', width: '100%' }}>
-        {<ResearcherSideBar />}
-      </div>
-    );
-  };
-  
-  export default MyResearch;
-  
+  return (
+    <Grid
+      container
+      sx={{ backgroundColor: '#F9F8EB', minHeight: '100%', width: '100%' }}
+    >
+      <Grid item>
+        <ResearcherSideBar />
+      </Grid>
+      <Grid item sx={{ margin: '1rem 1.5rem' }}>
+        <ResearcherProfileInfo name={'FirstName LastName'} />
+        <Typography sx={{ fontSize: '48px' }}>My Research</Typography>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default MyResearch;
