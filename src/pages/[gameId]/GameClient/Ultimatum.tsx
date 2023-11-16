3d2eb06d9ec378f00a023015283a43f220eb0b59
Cecil Cao <pokemonball34@gmail.com>
2023-11-16 18:54:53 -0500

    Use QLearningAgent

--- src/pages/[gameId]/GameClient/Ultimatum.tsx
+++ src/pages/[gameId]/GameClient/Ultimatum.tsx
@@ -149,7 +149,7 @@ const UltimatumLoader = () => {
     try {
       await axios
         .post(
-          '/games/start?gameType=ultimatum&player1_type=real&player2_type=minimax'
+          '/games/start?gameType=ultimatum&player1_type=real&player2_type=qlearningagent'
         )
         .then(() => {
           setIsLoading(false);
